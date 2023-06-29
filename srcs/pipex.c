dbf12343b7bffc5264efd4b1d97a410d9ac4ea25
Nicolas ROMANOFF <niromano@paul-f4Br3s1.clusters.42paris.fr>
2023-06-29 10:30:20 +0200

    protec file

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,11 +6,18 @@
 /*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 07:12:32 by niromano          #+#    #+#             */
-/*   Updated: 2023/06/29 10:17:02 by niromano         ###   ########.fr       */
+/*   Updated: 2023/06/29 10:26:17 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+void	file_error(int tube[2], t_list *list)
+{
+	close(tube[0]);
+	close(tube[1]);
+	system_error(list);
+}
 
 void	pipex_start(char **env, t_list *list, char *file[3], int tube[2])
 {
@@ -25,7 +32,7 @@ void	pipex_start(char **env, t_list *list, char *file[3], int tube[2])
 	{
 		fd = open(file[0], O_RDONLY);
 		if (fd == -1)
-			system_error(list);
+			file_error(tube, list);
 		cmd = set_cmd(list->content, env, list);
 		close(tube[0]);
 		dup2(fd, 0);
@@ -53,7 +60,7 @@ void	pipex_end(char **env, t_list *list, char *file[3], int tube[2])
 	{
 		fd = open(file[1], O_WRONLY | O_TRUNC | O_CREAT, 0644);
 		if (fd == -1)
-			system_error(temp);
+			file_error(tube, temp);
 		cmd = set_cmd(list->content, env, temp);
 		close(tube[1]);
 		dup2(tube[0], 0);
